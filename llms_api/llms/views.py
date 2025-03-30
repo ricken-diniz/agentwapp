35080ba6018807ac24e67814b4bc9ce3ab5ccec1
Ricken <dinizrickenn@gmail.com>
2025-03-30 00:00:16 -0300

    Implementing the RAG estructure in the webchatbot

--- llms_api/llms/views.py
+++ llms_api/llms/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from django.views.decorators.csrf import csrf_exempt
 from .controller import chain
-from .vectordatabase import run_query
+# from .vectordatabase import run_query
+from dbchroma import db
 import json
 
 chanels = {
@@ -14,10 +15,14 @@ def stream_model(user_input: json):
         messages = chanels[user_input['from']]
     else:
         messages = [
+            SystemMessage('Você é um Agente de Inteligência Artificial que deverá responder perguntas, de forma breve (até 2 linhas), de acordo com um contexto'),
             SystemMessage("Você é um agente responsável por responder mensagens de uma plataforma de mensages, haverá diferentes conversas e o sistema será responsável por passar os indentificadores de cada uma, para que cada uma possua apenas o contexto e histórico de sua própria conversa. Seu idioma nativo é o Português do Brasil."),
         ]
     prompt = user_input['body']
+    search = db.similarity_search(prompt, k=1)
+    res_vectordb = search[0].page_content
     messages.append(SystemMessage('O próximo prompt é uma mensagem de '+user_input['from']+' trate como contexto apenas as mensagens de mesma origem'))
+    messages.append(SystemMessage('Contexto: '+ res_vectordb))
     messages.append(HumanMessage(content=prompt,chanel=user_input['from']))
     
     chanels[user_input['from']] = messages
